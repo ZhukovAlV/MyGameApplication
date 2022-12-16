e1c825120cb03fb58f716169a95ab20959591f74
cooluser <avzhykov@gmail.com>
2022-12-16 10:54:45 +0600

    croup 134 init

--- app/src/main/java/com/example/mygameapplication/MainActivity.java
+++ app/src/main/java/com/example/mygameapplication/MainActivity.java
@@ -12,61 +12,27 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    private Spinner spinner;
     private TextView text;
-    private ImageView image;
 
-    /**
-     * Это callback метод при создании Activity
-     * @param savedInstanceState
-     */
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        // Наша View подключается
         setContentView(R.layout.activity_main);
 
-        // Добавили выпадающий список и текстовое поле
-        spinner = findViewById(R.id.spinnerGame);
-        text = findViewById(R.id.textView);
-        image = findViewById(R.id.imageView);
+        // Привязали ссылку text к id textDescription из xml файла
+        text = findViewById(R.id.textDescription);
     }
 
     /**
-     * Метод, который вызывается при нажатии на кнопку CHOOSE
-     * @param view
+     * Метод срабатывающий после нажатия кнопки
+     * @param view View
      */
     public void showDescription(View view) {
-        Log.i("info", "click Button showDescription: ");
-        int position = spinner.getSelectedItemPosition();
-
-        text.setText(getDescriptionByPosition(position));
-        image.setImageResource(getImageByPosition(position));
-
-        // Добавить яблоко
-      //  image.setImageResource(R.drawable.swipe);
-    }
-
-    /**
-     * Метод, выдающий описание по позиции
-     * @param pos позиция
-     * @return описание
-     */
-    public String getDescriptionByPosition(int pos) {
-        String[] descriptions = getResources().getStringArray(R.array.games_description);
-
-        return descriptions[pos];
-    }
-
-    /**
-     * Метод, выдающий описание по позиции
-     * @param pos позиция
-     * @return описание
-     */
-    public int getImageByPosition(int pos) {
-        // Достаем из массива картинок выбранную в спиннере
-        TypedArray images = getResources().obtainTypedArray(R.array.images);
-        return images.getResourceId(pos, 0);
+        Log.i("info", "Была нажата кнопка НАЖМИ");
+        text.setText("С Новым годом! Волшебства, \n" +
+                "Смеха, счастья и тепла, \n" +
+                "Мира, радостей, достатка \n" +
+                "И во всех делах порядка!");
     }
 }
